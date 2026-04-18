999dc52eafad998110e1545634c7f25467b84652
GSerdg <grebelsergey@mail.ru>
2026-04-18 11:49:31 +0300

    feat: add 07 test

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,67 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios, { AxiosInstance } from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('throttledGetDataFromApi', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+    jest.unmock('axios');
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.clearAllTimers();
+    throttledGetDataFromApi.cancel();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const mockAxiosInstance = {
+      get: jest.fn().mockResolvedValue({ data: {} }),
+    } as unknown as AxiosInstance;
+    mockedAxios.create.mockReturnValue(mockAxiosInstance);
+
+    const promise = throttledGetDataFromApi('/test');
+    jest.runAllTimers();
+    await promise;
+
+    expect(mockedAxios.create).toHaveBeenCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const relativePath = '/posts/1';
+    const mockAxiosInstance = {
+      get: jest.fn().mockResolvedValue({ data: {} }),
+    } as unknown as AxiosInstance;
+    mockedAxios.create.mockReturnValue(mockAxiosInstance);
+
+    const promise = throttledGetDataFromApi(relativePath);
+    jest.runAllTimers();
+    await promise;
+
+    expect(mockAxiosInstance.get).toHaveBeenCalledWith(relativePath);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const relativePath = '/posts/1';
+    const data = { id: 'string' };
+    const mockAxiosInstance = {
+      get: jest.fn().mockResolvedValue({ data }),
+    } as unknown as AxiosInstance;
+    mockedAxios.create.mockReturnValue(mockAxiosInstance);
+
+    const promise = throttledGetDataFromApi(relativePath);
+    jest.runAllTimers();
+    const responseData = await promise;
+
+    expect(responseData).toEqual(data);
   });
 });
